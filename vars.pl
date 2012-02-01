8e76968a50d0c9b959e463480f1893a6cb437d69
pyro <pyro@case.vs3.allsimple.net>
2012-02-01 15:16:12 +0000

    Added /undo and /redo functionality... this is scarily pro

--- vars.pl
+++ vars.pl
@@ -5,8 +5,8 @@ use Irssi::TextUI;
 BEGIN {
     use Storable;
     use Cwd;
-
-    our (%config, %foo, $loaded, $firsterr, $seconderr, $farg, $sarg, @varcmds);
+#build undo and redo stack
+    our (%config, %foo, $loaded, $firsterr, $seconderr, $farg, $sarg, @varcmds, @undo, @redo);
     my $hashref;
     
     Irssi::settings_add_str('vars', 'vardata_path', '/home/pyro/');
@@ -26,14 +26,47 @@ BEGIN {
         }
     }
 }
-our (%config, %foo, $loaded, $firsterr, $seconderr, $farg, $sarg, @varcmds, @tabcmds, @keys);
+our (%config, %foo, $loaded, $firsterr, $seconderr, $farg, $sarg, @varcmds, @tabcmds, @undo, @redo);
 our $pre;
 
-@varcmds = ('mkvar', 'rmvar', 'varlist', 'varhelp');
-@tabcmds = ('mkvar', 'rmvar');
+@varcmds = ('mkvar', 'rmvar', 'varlist', 'varhelp', 'undo', 'redo', 'editvar', 'cpvar');
+@tabcmds = ('mkvar', 'rmvar', 'editvar', 'cpvar');
+our $build = join('|', @tabcmds);
+
+sub stack_gen {
+    my($undoCode, $undoText, $redoCode, $redoText) = @_;
+
+    #first things first... since new action happening, clear redo buffer.
+    @redo = ();
+
+    my $ref = [[$undoCode, $undoText], [$redoCode, $redoText]];
+    push(@undo, $ref);
+}#build undo and redo stack
+
+sub undo {
+    if(!@undo) {
+        Irssi::print('No tasks left in the undo buffer');
+        return;
+    }
+    my $ref = pop(@undo);
+    &{ @{ @{$ref}[0] }[0] };
+    Irssi::print('Undo successful: ' . @{ @{$ref}[0] }[1]);
+    push(@redo, $ref); 
+}
+
+sub redo {
+    if(!@redo) {
+        Irssi::print('No tasks left in the redo buffer');
+        return;
+    }
+    my $ref = pop(@redo);
+    &{ @{ @{$ref}[1] }[0] };
+    Irssi::print('Redo successful: ' . @{ @{$ref}[1] }[1]);
+    push(@undo, $ref);
+}
 
 sub tab_complete {
-    my $testre = qr/^\/(mk|rm)var/; #only one regex to change.
+    my $testre = qr/^\/$build/; #only one regex to change.
     my ($strings, $window, $word, $linestart, $want_space) = @_;
     #Irssi::print("\$linestart: '$linestart'");
     #Irssi::print("\$word: '$word'");
@@ -41,14 +74,17 @@ sub tab_complete {
     my $brace;
     my $primed = 0;
     if ($linestart =~ /$testre/) {
+        #Irssi::print('condition 1');
         #build list of eligible words using $word.
         $post = $';
         #Irssi::print($post) if $post;
         return unless((!$post && $word !~ /\W/)
           ||(($post && $word =~ /\{\{((\w+)((?!\\\}\})|(?!\}\})))?$/) 
           && eval {$brace = 1; return 1}));
+          #Irssi::print("still alive!");
     }
     elsif($word =~ /\{\{((\w+)((?!\\\}\})|(?!\}\})))?$/) {
+        #Irssi::print('condition 2');
         $brace = 1;
     }
     else {
@@ -59,8 +95,11 @@ sub tab_complete {
     @$strings = (); #clear out any old rubbish that may be lingering
     $pre = '';
     foreach my $key (sort { lc($a) cmp lc($b) } keys %foo) {
-        $word =~ s/(.*)\{\{//;
-        $pre = $1 unless $pre; #get all the stuff in front of {{ just in case
+        $word =~ s/(.*)\{\{// if $brace;
+        $pre = $1 if ($1 and $brace); 
+        #Irssi::print("\$word: '$word'");
+        #get all the stuff in front of {{ just in case
+        #Irssi::print("pre: '$pre'");
         if($key =~ /^$word/i) {
             push(@$strings,
                 ($pre   ? $pre : '') .
@@ -86,8 +125,64 @@ sub cmd_mkvar {
     else {
         $farg = shift @args;
         $sarg = join(' ', @args);
+        if($foo{$farg}) {
+            Irssi::print("Error: variable '$farg' already exists. Use /editvar to overwrite");
+            return;
+        }
         if($farg =~ /\W/) {
             Irssi::print('Error: only alphanumeric characters (A-Z, a-z, 0-9 and _) are permitted in variable names.');
+            return;
+        }
+    }
+
+    while($sarg =~ /\G(?!\\)\{\{(\w+)(?!\\)\}\}/g) {
+        my $match = $1;
+        unless($foo{$match}) {
+            Irssi::print('Inserted variable \'{{' . $match . '}}\' does not exist. Remember to backlslash (\{{ \}}) any variables you do not want interpreted. Command failed.');
+            return;
+        }
+    }
+    my $safe = loopcheck($sarg);
+    if($safe eq 'ERROR') {
+        return;
+    }
+    else {
+        $foo{$farg} = $sarg;
+        Irssi::print("Variable '$farg' succesfully saved with value '$sarg'");
+        store(\%foo, $config{'vardata_path'}.'.vardata');
+        
+        #build undo and redo stack
+        my $undoRef = sub {
+                        delete $foo{$farg};
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $redoRef = sub {
+                        $foo{$farg} = $sarg;
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $undoTxt = "Deleted variable '$farg' containing value: '$sarg'";
+        my $redoTxt = "Recreated variable '$farg' containing value: '$sarg'";
+
+        stack_gen($undoRef, $undoTxt, $redoRef, $redoTxt);
+    }
+    return;
+}
+
+sub cmd_editvar {
+    my ($data) = @_;
+    $_ = $data;
+
+    my @args = split(/\s/);
+    if(scalar(@args) < 2) {
+        Irssi::print('Syntax Error: ' . (scalar(@args) == 1 ? 'Single' : 'No') . ' argument given.');
+        Irssi::print('Type /varhelp for command usage');
+        return;
+    }
+    else {
+        $farg = shift @args;
+        $sarg = join(' ', @args);
+        if(!$foo{$farg}) {
+            Irssi::print("Error: variable '$farg' does not exist. Use /mkvar to create a new variable.");
             return;
         }
     }
@@ -107,16 +202,123 @@ sub cmd_mkvar {
         Irssi::print('Definition of \'' . $farg . '\' unchanged');
     }
     else {
-        Irssi::print("Variable '$farg' succesfully saved with value '$sarg'");
+        Irssi::print("Variable '$farg' succesfully saved with new value '$sarg'");
         store(\%foo, $config{'vardata_path'}.'.vardata');
+
+        #build undo and redo stack
+        my $undoRef = sub {
+                        $foo{$farg} = $tmp;
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $redoRef = sub {
+                        $foo{$farg} = $sarg;
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $undoTxt = "Value of variable '$farg' reverted from '$sarg' to '$tmp'.";
+        my $redoTxt = "Value of variable '$farg' changed again from '$tmp' to '$sarg'";
+
+        stack_gen($undoRef, $undoTxt, $redoRef, $redoTxt);
+    }
+    return;
+}
+
+sub cmd_cpvar {
+    my ($data) = @_;
+    $_ = $data;
+
+    my @args = split(/\s/);
+    if(scalar(@args) < 2 || scalar(@args) > 3) {
+        Irssi::print('Syntax Error: ' . scalar(@args) . (scalar(@args) == 1 ? ' argument' : ' arguments') . 'given.');
+        Irssi::print('Type /varhelp for command usage');
+        return;
+    }
+
+    my $flag;
+    my $var = shift(@args);
+    if($var eq '-f') {
+        $flag = $var;
+        $var = shift(@args);
+    }
+    elsif($var =~ /\W/) {
+        Irssi::print("Error: '$var' is not a valid flag or variable name.");
+        Irssi::print('Type /varhelp for command usage');
+        return;
+    }
+    
+    unless($foo{$var}) {
+        Irssi::print("Error: variable '$var' does not exist. Cannot copy.");
+        return;
+    }
+    
+    my $newvar = shift(@args);
+    my $tmp;
+    if($foo{$newvar}) {
+        unless($flag) {
+            Irssi::print("Error, '$newvar' already exists. Use '-f' flag (/cpvar -f foo bar) if you wish to overwrite.");
+            return;
+        }
+        else {
+            $tmp = $foo{$newvar};
+            $foo{$newvar} = $foo{$var};
+            Irssi::print("Contents of variable '$var' successfully copied to existing variable '$newvar'.");
+            store(\%foo, $config{'vardata_path'}.'.vardata');
+        }
+    }
+    else {
+        $foo{$newvar} = $foo{$var};
+        Irssi::print("Contents of variable '$var' successfully copied to new variable '$newvar'.");
+        store(\%foo, $config{'vardata_path'}.'.vardata');
+    }
+
+    #build undo and redo stack
+    if($tmp) {
+        my $undoRef = sub {
+                        $foo{$newvar} = $tmp;
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $redoRef = sub {
+                        $foo{$newvar} = $foo{$var};
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $undoTxt = "Old value of variable '$newvar' restored from '".$foo{$var}."' to '$tmp'.";
+        my $redoTxt = "New value of variable '$newvar' restored from '$tmp' to '".$foo{$var}."'.";
+
+        stack_gen($undoRef, $undoTxt, $redoRef, $redoTxt);
+    }
+    else {
+        my $undoRef = sub {
+                        delete $foo{$newvar};
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $redoRef = sub {
+                        $foo{$newvar} = $foo{$var};
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $undoTxt = "Variable '$newvar' removed.";
+        my $redoTxt = "New variable '$newvar' restored with value '".$foo{$var}."'.";
+
+        stack_gen($undoRef, $undoTxt, $redoRef, $redoTxt);
     }
     return;
 }
 
 sub cmd_rmvar {
     my ($data) = @_;
+    my $tmp = $foo{$data};
     if(delete $foo{$data} && store(\%foo, $config{'vardata_path'}.'.vardata')) {
         Irssi::print("Variable '$data' has been successfully deleted");
+
+        #build undo and redo stack
+        my $undoRef = sub {
+                        $foo{$data} = $tmp;
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $redoRef = sub {
+                        delete $foo{$data};
+                        store(\%foo, $config{'vardata_path'}.'.vardata');
+                      };
+        my $undoTxt = "Deleted variable '$data' restored with value '$tmp'.";
+        my $redoTxt = "Variable '$data' deleted again.";
     }
     else {
         Irssi::print("Variable '$data' not found");
@@ -190,12 +392,12 @@ sub loopcheck {
 }
 
 sub listvars {
-    if(! %foo) {
+    if(!%foo) {
         Irssi::print('No variables found');
         return;
     }
     Irssi::print('Listing all variables:');
-    foreach my $key (sort keys %foo) {
+    foreach my $key (sort {lc($a) cmp lc($b)} keys %foo) {
         Irssi::print($key . ': \'' . $foo{$key} . '\'');
     }
 }
@@ -232,8 +434,12 @@ sub help {
 
 Irssi::command_bind('mkvar', 'cmd_mkvar');
 Irssi::command_bind('rmvar', 'cmd_rmvar');
+Irssi::command_bind('editvar', 'cmd_editvar');
+Irssi::command_bind('cpvar', 'cmd_cpvar');
 Irssi::command_bind('varhelp', 'help');
 Irssi::command_bind('varlist', 'listvars');
+Irssi::command_bind('undo', 'undo');
+Irssi::command_bind('redo', 'redo');
 #Irssi::signal_add('send text', 'varreplace');
 Irssi::signal_add('send command', 'varreplace');
 Irssi::signal_add_first("complete word", 'tab_complete');
